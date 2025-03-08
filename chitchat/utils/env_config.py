bb76662fc7c61df605385218ca0303ae46f67e11
shrikant.giri <shrikant.giri@coriolis.co.in>
2025-03-08 14:23:19 +0530

    Added config file and setpup for different enviroments

---
+++ chitchat/utils/env_config.py
@@ -0,0 +1,20 @@
+import configparser
+
+configs = configparser.ConfigParser()
+configs.read("./config/local.ini")
+configs.sections()
+
+
+class EnviromentConfigs:
+    # Default Settings
+    DEBUG_VALUE = configs["DEFAULT"]["DEBUG_VALUE"]
+    SECRET_KEY = configs["DEFAULT"]["SECRET_KEY"]
+    ALLOWED_HOSTS = configs["DEFAULT"]["ALLOWED_HOSTS"]
+
+    # Database Settings
+    ENGINE=configs["DATABASE"]["ENGINE"]
+    NAME=configs["DATABASE"]["NAME"]
+    USER=configs["DATABASE"]["USER"]
+    PASSWORD=configs["DATABASE"]["PASSWORD"]
+    HOST=configs["DATABASE"]["HOST"]
+    PORT=configs["DATABASE"]["PORT"]
