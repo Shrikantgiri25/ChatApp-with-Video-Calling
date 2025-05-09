142e1cf04b4a21133f6962383f0ae2d1b73ba695
shrikant.giri <shrikant.giri@coriolis.co.in>
2025-05-09 11:56:59 +0530

    changed the user is_active status=true when google login

--- chitchat/apps.py
+++ chitchat/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ChitchatConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chitchat"
+
+    def ready(self):
+
+        # Google login signal
+        # This will import the signal handlers when the app is ready    
+        import chitchat.signals.google_login
---
+++ chitchat/signals/google_login.py
@@ -0,0 +1,11 @@
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+
+@receiver(user_signed_up)
+def maybe_activate_google_user(request, user, **kwargs):
+    sociallogin = kwargs.get('sociallogin')
+    if sociallogin and sociallogin.account.provider == 'google':
+        extra_data = sociallogin.account.extra_data
+        if extra_data.get('email_verified'):
+            user.is_active = True
+            user.save()
