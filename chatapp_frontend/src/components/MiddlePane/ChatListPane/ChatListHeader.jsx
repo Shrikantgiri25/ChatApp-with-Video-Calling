34c720f22422a19bcb632dd4f782479112db9260
Shrikant Giri <shrikant.giri@coriolis.co.in>
2025-10-07 17:56:09 +0530

    Added kebab menu option for creating group and for other things

--- chatapp_frontend/src/components/MiddlePane/ChatListPane/ChatListHeader.jsx
+++ chatapp_frontend/src/components/MiddlePane/ChatListPane/ChatListHeader.jsx
@@ -1,11 +1,12 @@
-// ChatListHeader.jsx
-
-import React from 'react'
-import ChatListPane from './ChatListPane'
-import "./ChatListPane.scss"
-import { useDispatch, useSelector } from 'react-redux'
-import { selectConversationSearch } from '../../../store/selectors/searchSelector'
-import { CONVERSATIONLIST_SEARCH } from '../../../store/actiontypes/constants'
+import React from 'react';
+import ChatListPane from './ChatListPane';
+import "./ChatListPane.scss";
+import { useDispatch, useSelector } from 'react-redux';
+import { selectConversationSearch } from '../../../store/selectors/searchSelector';
+import { CONVERSATIONLIST_SEARCH } from '../../../store/actiontypes/constants';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import { Dropdown, Tooltip } from 'antd';
 
 const ChatListHeader = () => {
   const conversationSearch = useSelector(selectConversationSearch);
@@ -15,21 +16,30 @@ const ChatListHeader = () => {
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <div className="header-wrapper">
         <h2 className="chatapp-title">Chat Application</h2>
-        <input
-          className="searchText"
-          value={conversationSearch}
-          type="text"
-          placeholder="Search..."
-          onChange={(e) =>
-            dispatch({ type: CONVERSATIONLIST_SEARCH, payload: e.target.value })
-          }
-        />
+
+        {/* Search input and kebab menu in one line */}
+        <div className="search-wrapper">
+          <input
+            className="searchText"
+            value={conversationSearch}
+            type="text"
+            placeholder="Search..."
+            onChange={(e) =>
+              dispatch({ type: CONVERSATIONLIST_SEARCH, payload: e.target.value })
+            }
+          />
+          <Dropdown overlay={[]} trigger={['click']} placement="bottomRight">
+            <Tooltip title="More Options">
+              <FontAwesomeIcon className="kebab-icon" icon={faEllipsisVertical} />
+            </Tooltip>
+          </Dropdown>
+        </div>
       </div>
 
+      {/* Chat List Pane */}
       <ChatListPane search={conversationSearch} />
     </div>
   );
 };
 
-
-export default ChatListHeader
+export default ChatListHeader;
